b48227f721eaa745d1250eaf3e3d36199c5283ad
konsilerinos <n_slesarev@mail.ru>
2021-09-16 12:36:28 +0300

    Create Task_1.cpp

---
+++ Lab_1/Task_1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+int main()
+{
+	std::cout << "sizeof(bool) = " << sizeof(bool) << " bytes" << std::endl;
+	std::cout << "sizeof(char) = " << sizeof(char) << " bytes" << std::endl;
+	std::cout << "sizeof(signed char) = " << sizeof(signed char) << " bytes" << std::endl;
+	std::cout << "sizeof(unsigned char) = " << sizeof(unsigned char) << " bytes" << std::endl;
+	std::cout << "sizeof(char*) = " << sizeof(char*) << " bytes" << std::endl;
+	std::cout << "sizeof(wchar_t) = " << sizeof(wchar_t) << " bytes" << std::endl;
+	std::cout << "sizeof(wchar_t*) = " << sizeof(wchar_t*) << " bytes" << std::endl;
+	std::cout << "sizeof(short) = " << sizeof(short) << " bytes" << std::endl;
+	std::cout << "sizeof(short*) = " << sizeof(short*) << " bytes" << std::endl;
+	std::cout << "sizeof(int) = " << sizeof(int) << " bytes" << std::endl;
+	std::cout << "sizeof(int*) = " << sizeof(int*) << " bytes" << std::endl;
+	std::cout << "sizeof(long) = " << sizeof(long) << " bytes" << std::endl;
+	std::cout << "sizeof(long*) = " << sizeof(long*) << " bytes" << std::endl;
+	std::cout << "sizeof(long long) = " << sizeof(long long) << " bytes" << std::endl;
+	std::cout << "sizeof(long long*) = " << sizeof(long long*) << " bytes" << std::endl;
+	std::cout << "sizeof(float) = " << sizeof(float) << " bytes" << std::endl;
+	std::cout << "sizeof(float*) = " << sizeof(float*) << " bytes" << std::endl;
+	std::cout << "sizeof(double) = " << sizeof(double) << " bytes" << std::endl;
+	std::cout << "sizeof(double*) = " << sizeof(double*) << " bytes" << std::endl;
+	std::cout << "sizeof(long double) = " << sizeof(long double) << " bytes" << std::endl;
+	std::cout << "sizeof(long double*) = " << sizeof(long double*) << " bytes" << std::endl;
+
+
+	return 0;
+}
