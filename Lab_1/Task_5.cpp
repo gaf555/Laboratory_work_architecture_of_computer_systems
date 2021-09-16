3d54aa2240b3df810896cbee8b8e13d870af9f08
konsilerinos <n_slesarev@mail.ru>
2021-09-16 12:39:56 +0300

    Create Task_5.cpp

---
+++ Lab_1/Task_5.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <limits>
+
+void printDump(void*, size_t);
+
+int main() {
+
+	setlocale(LC_ALL, "russian");
+
+	int x = 0xA1B2C3D4, y = 3, z = -8;
+	int array[] = { x,y,z };
+	double dy = y;
+
+	char str1c[] = "bcdxy";
+	char str2c[] = "ёжзий";
+
+	wchar_t str1wc[] = L"bcdxy";
+	wchar_t str2wc[] = L"ёжзий";
+
+	std::cout << "x in int:" << std::endl;
+	printDump(&x, sizeof(x));
+
+	std::cout << "array[] in int:" << std::endl;
+	printDump(&array, sizeof(array));
+
+	std::cout << "str1c in char:" << std::endl;
+	printDump(&str1c, sizeof(str1c));
+
+	std::cout << "str2c in char:" << std::endl;
+	printDump(&str2c, sizeof(str2c));
+	
+	std::cout << "str1wc in char:" << std::endl;
+	printDump(&str1wc, sizeof(str1wc));
+
+	std::cout << "str2wc in char:" << std::endl;
+	printDump(&str2wc, sizeof(str2wc));
+
+	return 0;
+}
+
+void printDump(void *p, size_t N) {
+
+	unsigned char* pbtP = (unsigned char*)(p);
+
+	for (int i = 0; i < N; i++) {
+
+		std::cout << (int)*(pbtP + i) << std::endl;
+	}
+	std::cout << std::endl;
+}
