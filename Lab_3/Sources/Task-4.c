d136008754b88bdeda5c03c3f09274451bb9bf06
konsilerinos <n_slesarev@mail.ru>
2021-10-14 01:35:21 +0300

    Update Task-4.c

--- Lab_3/Sources/Task-4.c
+++ Lab_3/Sources/Task-4.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
 
+int main() {
+
+    const int N = 10;
+
+    int mas[N];
+    int k = 0, x = 0;
+
+    for(int i=0;i<N;i++){
+
+        mas[i] = 0;
+        printf("%d ", mas[i]);
+    }
+    printf("\n");
+
+    printf("Enter k: ");
+    scanf("%d", &k);
+
+    printf("Enter x: ");
+    scanf("%d", &x);
+
+    // mas[k] = x
+    asm
+    (
+        "movl	-84(%rbp), %edx\n"
+        "movl	-88(%rbp), %ecx\n"
+        "movq	-80(%rbp), %rax\n"
+        "movslq	%edx, %rdx\n"
+        "movl	%ecx, (%rax,%rdx,4)\n"
+    );
+
+    for(int i=0;i<N;i++){
+
+        printf("%d ", mas[i]);
+    }
+    printf("\n");
+
+    printf("Press <Enter> to continue...");
+
+    return 0;
+}
