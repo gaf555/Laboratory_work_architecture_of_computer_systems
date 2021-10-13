9e6ef1c931693837ceafe0cf6ee9ab45dfedefbd
konsilerinos <n_slesarev@mail.ru>
2021-10-14 02:15:54 +0300

    Update Task-3.c

--- Lab_3/Sources/Task-3.c
+++ Lab_3/Sources/Task-3.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main()
+{
+	int x, y, w, z, trash;
+
+	scanf("%d", &x);
+	scanf("%d", &y);
+
+
+	asm(
+		"movl $79,%%eax\n"
+		"addl %2,%%eax\n"
+		"idiv %3\n"
+		"movl %%al , %0\n"
+		"movl %%ah , %1"
+		:"+r"(w), "+r"(z)
+		: "r"(x), "r"(y)
+		: "cc"
+	);
+
+	return 0;
+}
